55155cde211824281e8ee4feaff4a82b2866f014
SudiptaSundarPramanik <79316024+SudiptaSundarPramanik@users.noreply.github.com>
2021-09-20 19:57:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,26 @@ function preload(){
 
 }
 function setup(){
-    canvas=createCanvas(640,480);
-    canvas.position(300,280);
+    canvas=createCanvas(640,450);
+    canvas.position(300,200);
     video=createCapture(VIDEO);
     video.hide();
     tint_color="";
 }
 function draw(){
-    image(video,0,0,640,480);
+    image(video,230,150,220,200);
+    stroke(125,250,100);
+    fill(50,150,200);
+    circle(50,50,100);
+    circle(50,430,100);
+    circle(590,50,100);
+    circle(590,430,100);
+    stroke(243,98,160);
+    fill(150,250,100);
+    rect(20,90,60,300);
+    rect(560,90,60,300);
+    rect(100,30,440,50);
+    rect(100,400,440,50);
     tint(tint_color);
 }
 function take_snapshot(){
